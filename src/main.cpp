4f0105c4e8d1394c1233c3e69fac6f6a197399a1
olin_tp_ar <universalexperimental@protonmail.com>
2023-04-13 17:06:58 +0200

    listen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../header/webserv.hpp"
 #include "../header/colours.hpp"
+#include <sys/socket.h>
 
 void	error_handle(std::string type)
 {
@@ -18,9 +19,9 @@ int main(void)
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		error_handle("Couldnt create socket.");
+
 	//2. name the socket
 	//assigning a transport address to the socket, a port number. use bind() syscall.
-	//int bind(int socket, const struct sockaddr *address, socklen_t address_len)
 	//filling sockaddr_in (structure describing an internet socket address) struct before bind()
 	struct		sockaddr_in address;
 	const int	port = 8080;
@@ -31,11 +32,18 @@ int main(void)
 	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	//htons converts a short integer to a network representation
 	address.sin_port = htons(port);
+
 	//sockaddr (structure describing a generic socket address) struct before bind()
+	//int bind(int socket, const struct sockaddr *address, socklen_t address_len)
 	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0)
 		error_handle("Binding error");
 
-	//3. wait for an incoming connection
+	//3. listen /  wait for an incoming connection
+	// listen (int socket, int backlog) puts the server socket in a passive mode
+	// backlog defines the max # of pending connection that can queue b4 being refued with (ECONNREFUSED)
+	if (listen(server_fd, 3) < 0)
+		error_handle("listen");
+	//
 	//4. send and receive messages
 	//5. close the socket
 	return (0);
