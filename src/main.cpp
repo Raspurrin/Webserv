ade57ea8d5f5d134d985e6925493a683754ad4c6
olin_tp_ar <universalexperimental@protonmail.com>
2023-04-13 16:41:35 +0200

    filling sockaddr struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "../header/webserv.hpp"
 #include "../header/colours.hpp"
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 int main(void)
 {
 	//programming wtih tcp/ip sockets
 	//1. create the socket using the system call socket(domain, type, protocol)
-	//domain: AF_INET macro for IP
+	//domain or address family: AF_INET macro for IP
 	//type: SOCK_STREAM virtual circuit service TCP
 	//protocol: 0 IP
 	int	server_fd;
@@ -17,6 +18,17 @@ int main(void)
 		exit(0);
 	}
 	//2. name the socket
+	//assigning a transport address to the socket, a port number. use bind() syscall.
+	//int bind(int socket, const struct sockaddr *address, socklen_t address_len)
+	//filling sockaddr struct before bind()
+	struct		sockaddr_in address;
+	const int	port = 8080;
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY; //0.0.0.0 OS chooses
+	address.sin_port = htons(port); //htons converts a short integer to a network representation
+	
+	//
 	//3. wait for an incoming connection
 	//4. send and receive messages
 	//5. close the socket
