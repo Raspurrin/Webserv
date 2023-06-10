0ea21bb7c1c8a108d27b512d7c63b86ae42532f9
fhenrich <fhenrich@student.42wolfsburg.de>
2023-06-10 09:35:44 +0200

    Add exception wrapper for Error enum.

--- header/error.h
+++ header/error.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <exception>
 
 enum Error {
 	Bad_Request,
 	Forbidden,
 	Not_Found,
 	Internal_Error
+};
+
+class ErrC: public std::exception {
+private:
+	Error err;
+
+public:
+	ErrC(Error);
+
+	Error getError() const;
 };
--- src/error.cpp
+++ src/error.cpp
@@ -1,7 +1,14 @@
 #include "../header/webserv.hpp"
+#include "../header/error.h"
 
 void	error_handle(std::string type)
 {
 	perror(type.c_str());
 	exit(0);
+}
+
+ErrC::ErrC(Error err): err(err) {}
+
+Error ErrC::getError() const {
+	return err;
 }
