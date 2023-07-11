df62db7bfc32c1fafd7a3d93dc0da8ef006f62d9
Frederic Henrichs <F.Henrichs@web.de>
2023-07-11 15:16:48 +0200

    Add new cgi testfiles.

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,5 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+import time;
+import cgi, cgitb;
+import os
+
+form = cgi.FieldStorage()
+first_name = form.getvalue("first_name")
+last_name = form.getvalue("last_name")
 print("Content-Type: text/html")
-hw = "Hello World"
-print("Content-Length: %i\n" % len(hw))
+hw = "<p>First Name: {fn}, Last Name: {ln}</p>".format(fn = first_name, ln = last_name)
+print(f"Content-Length: {len(hw) + 1}")
+
+print("\r\n\r")
 print(hw)
+# for k, v in os.environ.items():
+#     print(f'<p>{k}={v}</p>')
