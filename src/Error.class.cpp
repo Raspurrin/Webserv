6943a53a9bf68baa7c2c91ae2fcb8268cbcb261e
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-26 13:01:39 +0200

    changing naming in error class implementation

---
+++ src/Error.class.cpp
@@ -0,0 +1,20 @@
+#include "../header/webserv.hpp"
+#include "../header/Error.class.hpp"
+
+void	error_handle(std::string type)
+{
+	perror(type.c_str());
+	exit(0);
+}
+
+ErrorResponse::ErrorResponse(ErrorType _errorType): _errorType(_errorType), _description("No description provided") {}
+
+ErrorResponse::ErrorResponse(ErrorType _errorType, std::string _description): _errorType(_errorType), _description(_description) {}
+
+ErrorType ErrorResponse::getError() const throw() {
+	return _errorType;
+}
+
+const char *ErrorResponse::what() const throw() {
+	return _description.c_str();
+}
