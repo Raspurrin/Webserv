3e955f3cda003bb88749771775ca497ae4d032bf
olin_tp_ar <universalexperimental@protonmail.com>
2023-08-09 22:13:24 +0200

    int for client index in handle client socket

--- src/ServerManager.class.cpp
+++ src/ServerManager.class.cpp
@@ -1,189 +1,190 @@
-	#include "../header/ServerManager.class.hpp"
-	#include "../header/ServerConfigParser.class.hpp"
-	#include "../header/Cgi.class.hpp"
+#include "../header/ServerManager.class.hpp"
+#include "../header/ServerConfigParser.class.hpp"
+#include "../header/Cgi.class.hpp"
 
-	IntVector	_indexesToRemove;
+IntVector	_indexesToRemove;
 
-	ServerManager::ServerManager(void) :
-		_opt(1), _numServerSockets(0)
-	{
-		ServerConfigParser	parsingConfig("configuration.conf");
+ServerManager::ServerManager(void) :
+	_opt(1), _numServerSockets(0)
+{
+	ServerConfigParser	parsingConfig("configuration.conf");
 
-		_serverConfigs = parsingConfig.getServerConfigs();
-		addServerSockets();
-	}
+	_serverConfigs = parsingConfig.getServerConfigs();
+	addServerSockets();
+}
 
-	void	ServerManager::addServerSockets(void)
-	{
-		for (std::vector<ServerConfig>::iterator it = _serverConfigs.begin(); it != _serverConfigs.end(); it++)
-		{
-			if (DEBUG)
-				it->printServerConfig();
-			addServerSocket(*it);
-		}
-	}
-
-	void	ServerManager::addServerSocket(ServerConfig &serverConfig)
-	{
-		int	listen_socket_fd;
-
-		if ((listen_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-			error_handle("Socket error in webserver");
-
-		if (DEBUG)
-			std::cout << "- addServerSocket: Socket " << listen_socket_fd << std::endl;
-
-		configureSocket(listen_socket_fd);
-
-		if (setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, &_opt, sizeof(_opt)))
-		{
-			close(listen_socket_fd);
-			error_handle("setsockopt");
-		}
-
-		if (bind(listen_socket_fd, reinterpret_cast<struct sockaddr *>(&serverConfig.getAddress()), sizeof(serverConfig.getAddress())) < 0)
-		{
-			close(listen_socket_fd);
-			error_handle("Binding error");
-		}
-
-		if (listen(listen_socket_fd, BACKLOG) < 0)
-		{
-			close(listen_socket_fd);
-			error_handle("Listen error");
-		}
-
-		t_pollfd serverSocket;
-		serverSocket.fd = listen_socket_fd;
-		serverSocket.events = POLLIN;
-
-		_sockets.push_back(serverSocket);
-		++_numServerSockets;
-	}
-
-	void	ServerManager::configureSocket(int newSocket)
+void	ServerManager::addServerSockets(void)
+{
+	for (std::vector<ServerConfig>::iterator it = _serverConfigs.begin(); it != _serverConfigs.end(); it++)
 	{
 		if (DEBUG)
-			std::cout << "- configureSocket: Socket " << newSocket << std::endl;
+			it->printServerConfig();
+		addServerSocket(*it);
+	}
+}
 
-		int		flags;
+void	ServerManager::addServerSocket(ServerConfig &serverConfig)
+{
+	int	listen_socket_fd;
 
-		flags = fcntl(newSocket, F_GETFL, 0);
-		if (flags == -1)
-			error_handle("Failed to get socket flags in fcntl().");
-		if (fcntl(newSocket, F_SETFL, flags | O_NONBLOCK) == -1)
-			error_handle("Failed to set socket to non-blocking in fcntl().");
+	if ((listen_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		error_handle("Socket error in webserver");
+
+	if (DEBUG)
+		std::cout << "- addServerSocket: Socket " << listen_socket_fd << std::endl;
+
+	configureSocket(listen_socket_fd);
+
+	if (setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, &_opt, sizeof(_opt)))
+	{
+		close(listen_socket_fd);
+		error_handle("setsockopt");
 	}
 
-	void	ServerManager::eventLoop(void)
+	if (bind(listen_socket_fd, reinterpret_cast<struct sockaddr *>(&serverConfig.getAddress()), sizeof(serverConfig.getAddress())) < 0)
 	{
-		int	numEvent;
+		close(listen_socket_fd);
+		error_handle("Binding error");
+	}
 
-		while (!g_shutdown_flag)
+	if (listen(listen_socket_fd, BACKLOG) < 0)
+	{
+		close(listen_socket_fd);
+		error_handle("Listen error");
+	}
+
+	t_pollfd serverSocket;
+	serverSocket.fd = listen_socket_fd;
+	serverSocket.events = POLLIN;
+
+	_sockets.push_back(serverSocket);
+	++_numServerSockets;
+}
+
+void	ServerManager::configureSocket(int newSocket)
+{
+	if (DEBUG)
+		std::cout << "- configureSocket: Socket " << newSocket << std::endl;
+
+	int		flags;
+
+	flags = fcntl(newSocket, F_GETFL, 0);
+	if (flags == -1)
+		error_handle("Failed to get socket flags in fcntl().");
+	if (fcntl(newSocket, F_SETFL, flags | O_NONBLOCK) == -1)
+		error_handle("Failed to set socket to non-blocking in fcntl().");
+}
+
+void	ServerManager::eventLoop(void)
+{
+	int	numEvent;
+
+	while (!g_shutdown_flag)
+	{
+		numEvent = poll(_sockets.data(), _sockets.size(), -1);
+		if (numEvent == -1)
 		{
-			numEvent = poll(_sockets.data(), _sockets.size(), -1);
-			if (numEvent == -1)
+			perror("Poll error");
+			break ;
+		}
+		else if (numEvent == 0)
+			continue ;
+		for (size_t i = 0; i < _sockets.size(); ++i)
+		{
+			if (i < (size_t)_numServerSockets)
 			{
-				perror("Poll error");
-				break ;
+				listenToServerSocket(i);
+				if (g_shutdown_flag)
+					break ;
 			}
-			else if (numEvent == 0)
-				continue ;
-			for (size_t i = 0; i < _sockets.size(); ++i)
-			{
-				if (i < (size_t)_numServerSockets)
-				{
-					listenToServerSocket(i);
-					if (g_shutdown_flag)
-						break ;
-				}
-				else {
-					handleClientSocket(i);
-				}
-				removeIndexes();
+			else {
+				handleClientSocket(i);
 			}
+			removeIndexes();
 		}
 	}
+}
 
-	void	ServerManager::listenToServerSocket(int i)
+void	ServerManager::listenToServerSocket(int i)
+{
+	if (_sockets[i].revents & POLLIN)
+		addClientSocket(_sockets[i], _serverConfigs[i]);
+}
+
+void	ServerManager::addClientSocket(t_pollfd &serverSocket, ServerConfig &serverConfig)
+{
+	int	newClientSocket;
+
+	newClientSocket = accept(serverSocket.fd, NULL, NULL);
+	if (newClientSocket == -1)
+		perror("Accept error");
+	else
 	{
-		if (_sockets[i].revents & POLLIN)
-			addClientSocket(_sockets[i], _serverConfigs[i]);
+		configureSocket(newClientSocket);
+		setsockopt(newClientSocket, IPPROTO_TCP, TCP_NODELAY, &_opt, sizeof(_opt));
+
+		pollfd	newPfd;
+		newPfd.fd = newClientSocket;
+		newPfd.events = POLLIN | POLLOUT | POLLERR;
+		newPfd.revents = 0;
+		_sockets.push_back(newPfd);
+		Client	newClient(newPfd, serverConfig);
+		_clients.push_back(newClient);
 	}
+}
 
-	void	ServerManager::addClientSocket(t_pollfd &serverSocket, ServerConfig &serverConfig)
-	{
-		int	newClientSocket;
+void	ServerManager::sendResponse(Client &client)
+{
+	std::string	_response = client.getResponse();
+	static int i = 0;
 
-		newClientSocket = accept(serverSocket.fd, NULL, NULL);
-		if (newClientSocket == -1)
-			perror("Accept error");
-		else
-		{
-			configureSocket(newClientSocket);
-			setsockopt(newClientSocket, IPPROTO_TCP, TCP_NODELAY, &_opt, sizeof(_opt));
+	if (DEBUG)
+		std::cout << YELLOW << "========= " << DEF << "Sending response: " << i << YELLOW << " =========\n\n" << DEF;
 
-			pollfd	newPfd;
-			newPfd.fd = newClientSocket;
-			newPfd.events = POLLIN | POLLOUT | POLLERR;
-			newPfd.revents = 0;
-			_sockets.push_back(newPfd);
-			Client	newClient(newPfd, serverConfig);
-			_clients.push_back(newClient);
-		}
+	int err = send(client.getSocket(),_response.c_str(), _response.length(), 0);
+
+	if (DEBUG)
+		std::cout << RED << "------> SUCCESS: Message sent from server. <-------\n\n" << DEF;
+
+	if (client.responseFinished() || err == -1 || (size_t)err != _response.length()) {
+		close(client.getSocket());
 	}
+	i++;
+}
 
-	void	ServerManager::sendResponse(Client &client)
+void	ServerManager::handleClientSocket(int i)
+{
+	int	clientIndex = i - _numServerSockets;
+
+	if (!_clients[clientIndex].isRequestSent() && time(NULL) - _clients[clientIndex].getLastActivity() > REQUEST_TIMEOUT) {
+		_clients[clientIndex].setRequestError(ErrorResponse(408, "from ServerManager"));
+	}
+	else if (_sockets[i].revents & POLLERR || _sockets[i].revents & POLLHUP || _sockets[i].revents & POLLPRI || _sockets[i].revents & POLLNVAL) {
+		_indexesToRemove.push_back(i);
+	}
+	else if (_sockets[i].revents & POLLIN && !_clients[clientIndex].isRequestSent())
 	{
-		std::string	_response = client.getResponse();
-		static int i = 0;
-
 		if (DEBUG)
-			std::cout << YELLOW << "========= " << DEF << "Sending response: " << i << YELLOW << " =========\n\n" << DEF;
-
-		int err = send(client.getSocket(),_response.c_str(), _response.length(), 0);
-
-		if (DEBUG)
-			std::cout << RED << "------> SUCCESS: Message sent from server. <-------\n\n" << DEF;
-
-		if (client.responseFinished() || err == -1 || (size_t)err != _response.length()) {
-			close(client.getSocket());
-		}
-		i++;
+			std::cout << "- POLLIN with index " << i << " fd is " << _sockets[i].fd << std::endl;
+		_clients[clientIndex].getRequest(i);
 	}
-
-	void	ServerManager::handleClientSocket(int i)
-	{
-
-		if (!_clients[i - _numServerSockets].isRequestSent() && time(NULL) - _clients[i - _numServerSockets].getLastActivity() > REQUEST_TIMEOUT) {
-			_clients[i - _numServerSockets].setRequestError(ErrorResponse(408, "from ServerManager"));
-		}
-		if (_sockets[i].revents & POLLERR || _sockets[i].revents & POLLHUP || _sockets[i].revents & POLLPRI || _sockets[i].revents & POLLNVAL) {
+	else if (_clients[clientIndex].isRequestSent() && _sockets[i].revents & POLLOUT) {
+		sendResponse(_clients[clientIndex]);
+		if (_clients[clientIndex].responseFinished()) {
 			_indexesToRemove.push_back(i);
 		}
-		else if (_sockets[i].revents & POLLIN && !_clients[i - _numServerSockets].isRequestSent())
-		{
-			if (DEBUG)
-				std::cout << "- POLLIN with index " << i << " fd is " << _sockets[i].fd << std::endl;
-			_clients[i - _numServerSockets].getRequest(i);
-		}
-		else if (_clients[i - _numServerSockets].isRequestSent() && _sockets[i].revents & POLLOUT) {
-			sendResponse(_clients[i - _numServerSockets]);
-			if (_clients[i - _numServerSockets].responseFinished()) {
-				_indexesToRemove.push_back(i);
-			}
-		}
 	}
+}
 
-	void	ServerManager::removeIndexes()
-	{
-		for (size_t i = 0; i < _indexesToRemove.size(); ++i) {
-			_sockets.erase(_sockets.begin() + _indexesToRemove[i]);
-			_clients.erase(_clients.begin() + (_indexesToRemove[i] - _numServerSockets));
-		}
-		_indexesToRemove.clear();
+void	ServerManager::removeIndexes()
+{
+	for (size_t i = 0; i < _indexesToRemove.size(); ++i) {
+		_sockets.erase(_sockets.begin() + _indexesToRemove[i]);
+		_clients.erase(_clients.begin() + (_indexesToRemove[i] - _numServerSockets));
 	}
+	_indexesToRemove.clear();
+}
 
-	ServerManager::~ServerManager(void)
-	{
-	}
+ServerManager::~ServerManager(void)
+{
+}
