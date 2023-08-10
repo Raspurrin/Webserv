e9b7daacd031ef64b50b7082d3237f1cea249795
fhenrich <fhenrich@student.42wolfsburg.de>
2023-08-10 13:41:42 +0200

    simplify perl script.

--- www/cgi-bin/test.pl
+++ www/cgi-bin/test.pl
@@ -2,16 +2,9 @@
 use CGI;
 
 my $cgi = CGI->new();
-my %param = map { $_ => scalar $cgi->param($_) } $cgi->param();
 
 print $cgi->header;
-# print qq{PARAM:\n};
-for my $k ( sort keys %param ) {
-	print "<p>";
-	print join ": ", $k, $param{$k};
-	print "</p>\n"
-}
 
 print << 'END';
-<p> Hello World </p>
+Hello World
 END
